2751b891d2dfa0dc8d5ab3f0a4f7d443702fbfd9
isabelv4807 <56267096+isabelv4807@users.noreply.github.com>
2019-10-08 11:53:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "isabel velazquez"
+let userName = "isabelv4807";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
